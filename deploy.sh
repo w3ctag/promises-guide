e95eaef65af6e9e1f988033f9dbf6da4537e21a0
Domenic Denicola <d@domenic.me>
2019-07-16 16:49:32 -0400

    Fix pull request CI building

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ function doCompile {
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
+    mkdir -p out
     doCompile
     exit 0
 fi
